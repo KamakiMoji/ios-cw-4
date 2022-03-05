a71117697a49e56322bf5bd7d000ecd692252451
Mohammed Feras Al Bahar <100701492+KamakiMoji@users.noreply.github.com>
2022-03-05 13:34:44 +0300

    finished 4

---
+++ Mohammed's Playground.playground/Contents.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+let name = "Mohammed"
+var hobby = "playing any kind of sport"
+var favoritenumber = 11
+print(" My name is \(name), my favorite number is \(favoritenumber) and I like \(hobby) ")
+var height = 168.5
